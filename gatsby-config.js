f56f6fcb0c8b07f0a953eb3a9bc9f22ae60e14be
william <williamimoh@yahoo.com>
2020-05-07 18:53:37 +0400

    modified manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,12 +35,12 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Ten Brushes Gallery`,
+                short_name: `Ten Brushes`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
+                background_color: `#082254`,
+                theme_color: `#082254`,
+                display: `fullscreen`,
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
         },
